7aeb3f2f8620361c9001c75b32dd36778479f6b5
khoa <hatgiong@gmail.com>
2022-02-04 16:29:48 +0700

    Initial commit

---
+++ lib/common/dio_client.dart
@@ -0,0 +1,37 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_app_sale/data/datasources/local/share_pref.dart';
+
+class DioClient{
+  Dio? _dio;
+  static final BaseOptions _options = BaseOptions(
+    baseUrl: "https://freeapi.code4func.com/api/v1/",
+    connectTimeout: 5000,
+    receiveTimeout: 3000,
+  );
+
+  static final DioClient instance = DioClient._internal();
+
+  DioClient._internal() {
+    if (_dio == null){
+      _dio = Dio(_options);
+      // _dio!.interceptors.add(LogInterceptor(requestBody: true));
+      _dio!.interceptors.add(InterceptorsWrapper(
+        onRequest: (options, handler) async{
+          var token = await SharePre.instance.get("token");
+          if (token != null) {
+            options.headers["Authorization"] = "Bearer " + token;
+          }
+          return handler.next(options);
+        },
+        onResponse: (e, handler) {
+          return handler.next(e);
+        },
+        onError: (e, handler) {
+          return handler.next(e);
+        },
+      ));
+    }
+  }
+
+  Dio get dio => _dio!;
+}
